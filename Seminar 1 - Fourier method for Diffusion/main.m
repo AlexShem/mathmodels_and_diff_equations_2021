995174a3fc9c7e3da23497dcd6ea57aa50cfc1f8
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2021-11-03 21:59:10 +0100

    Basis sin(pi k / L) update

--- Seminar 1 - Fourier method for Diffusion/main.m
+++ Seminar 1 - Fourier method for Diffusion/main.m
@@ -8,9 +8,9 @@ x = linspace(0, L, 101);
 t = linspace(0, T, 201);
 [x, t] = meshgrid(x, t);
 
-u_0 = @(x) sin(2*pi/L * x);
+u_0 = @(x) sin(pi/L * x);
 C = zeros(K, 1);
-e_k = @(x, k) sin(2*pi*k/L*x);
+e_k = @(x, k) sin(pi*k/L*x);
 
 u = zeros(size(x));
 
@@ -18,7 +18,7 @@ for k = 1 : K
     fe = integral(@(x) u_0(x) .* e_k(x, k), 0, L);
     ee = integral(@(x) e_k(x, k).^2, 0, L);
     C(k) = fe / ee;
-    u = u + C(k) * exp(-D * (2*pi*k/L)^2 * t) .* sin(2*pi*k/L * x);
+    u = u + C(k) * exp(-D * (pi*k/L)^2 * t) .* e_k(x, k);
     u_true = u + a + (b - a)/L * x;
 end
 

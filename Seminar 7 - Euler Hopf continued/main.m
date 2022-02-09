7d22f516c498fc7c1ad3ba6d368c185f37d093bc
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2022-02-09 21:19:28 +0100

    Seminar7 - Order of the compact scheme

--- Seminar 7 - Euler Hopf continued/main.m
+++ Seminar 7 - Euler Hopf continued/main.m
@@ -1,25 +1,44 @@
 %% Main parameters
 L = 2*pi;
-T = .99;
-nu = 0.1; % nu = tau/h
-Nx = 200;
-x = linspace(0, L, Nx + 1);
-h = x(2) - x(1);
-tau = nu * h;
+T = .5;
+nu = 0.05; % nu = tau/h
+Nx = [25, 50, 100, 200];
+% Nx = 25 : 25 : 200;
 
 u_0 = @(x) sin(x);
 f = @(u) u.^2/2;
 
-ax = [0 L -2 2];
+Nx_ref = 400;
+h_ref = L / (Nx_ref - 1);
+tau_ref = nu * h_ref;
+[U_ref, ~, ~, x_ref, ~, t_ref] = EH_integration(L, T, Nx_ref, tau_ref, u_0, f, 'compact');
+[x_ref, t_ref] = meshgrid(x_ref, t_ref);
 
 %%
-[U_ref, ~, ~, x_ref, ~, t_ref] = EH_integration(L, T, 400, tau, u_0, f, 'compact');
+C_norm = zeros(size(Nx));
+figure(1);
 
-%% Visualisation
-figure(2)
-for k = [1 : floor(Nt/200) : Nt, Nt]
-    plot(x, U(k, :));
-    axis(ax);
-    title(['t = ', num2str((k-1)*tau)]);
-    drawnow;
+for j = 1 : length(Nx)
+    x = linspace(0, L, Nx(j) + 1);
+    h = x(2) - x(1);
+    tau = nu * h;
+    
+    [U, ~, ~, x, ~, t] = EH_integration(L, T, Nx(j), tau, u_0, f, 'compact');
+    [x_mesh, t_mesh] = meshgrid(x, t);
+    U_ref_val = interp2(x_ref, t_ref, U_ref, x_mesh, t_mesh);
+    Cn = max(abs(U_ref_val - U), [], 2);
+    plot(t, Cn); hold on;
+    
+    Cn = Cn(~isnan(Cn));
+    C_norm(j) = Cn(end);
 end
+hold off;
+
+%%
+figure(2);
+loglog(Nx, C_norm);
+
+%%
+order_comp = (log10(C_norm(end)) - log10(C_norm(end-2))) / ...
+    (log10(Nx(end)) - log10(Nx(end - 2)));
+disp(['[ORDER] Compact scheme: ', num2str(order_comp), '. nu = ', num2str(nu)])

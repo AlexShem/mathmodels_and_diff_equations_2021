d36658844f0a61d361200be821c51d7f5ce76bbd
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2022-05-18 18:43:11 +0200

    [Upd] added b coeff

--- Seminar 15 - 2D Poisson equation/system_poisson.m
+++ Seminar 15 - 2D Poisson equation/system_poisson.m
@@ -30,11 +30,16 @@ F = p*eye(N);
 % X Left border: first
 A(1, 1 + Ny) = a; % right
 A(1, 2) = a; % down
-A(1, 1 + (Nx-1)*Ny) = a; %left
+A(1, 1 + N - Ny) = a; %left
 A(1, Ny) = a; %up
+A(1, 1 + 2*Ny - 1) = b; % tr
+A(1, 1 + Ny + 1) = b; % br
+A(1, N - Ny + 2) = b; % bl
+A(1, N) = b; % tl
+
 F(1, 1 + Ny) = q; % right
 F(1, 2) = q; % down
-F(1, 1 + (Nx-1)*Ny) = q; %left
+F(1, 1 + N - Ny) = q; %left
 F(1, Ny) = q; %up
 
 % X Left border: intermediate
@@ -42,31 +47,46 @@ for k = 2 : Ny-1
     % a coeffs
     A(k, k + Ny) = a; % right
     A(k, k + 1) = a; % down
-    A(k, k + (Nx-1)*Ny) = a; %left
+    A(k, k + N - Ny) = a; %left
     A(k, k - 1) = a; %up
+    A(k, k + Ny - 1) = b; % tr
+    A(k, k + Ny + 1) = b; % br
+    A(k, k + N - Ny + 1) = b; % bl
+    A(k, k + N - Ny - 1) = b; % tl
+    
     F(k, k + Ny) = q; % right
     F(k, k + 1) = q; % down
-    F(k, k + (Nx-1)*Ny) = q; %left
+    F(k, k + N - Ny) = q; %left
     F(k, k - 1) = q; %up
 end
 
 % X Left border: last
 A(Ny, 2*Ny) = a; % right
 A(Ny, 1) = a; % down
-A(Ny, Ny + (Nx-1)*Ny) = a; %left
+A(Ny, Ny + N - Ny) = a; %left
 A(Ny, Ny - 1) = a; %up
+A(Ny, Ny + Ny - 1) = b; % tr
+A(Ny, Ny + 1) = b; % br
+A(Ny, N - Ny + 1) = b; % bl
+A(Ny, N - 1) = b; % tl
+
 F(Ny, 2*Ny) = q; % right
 F(Ny, 1) = q; % down
-F(Ny, Ny + (Nx-1)*Ny) = q; %left
+F(Ny, Ny + N - Ny) = q; %left
 F(Ny, Ny - 1) = q; %up
 
 %% Intermediate X steps
-for k = Ny+1 : (Nx-1)*Ny
+for k = Ny+1 : N-Ny
     if mod(k, Ny) == 1 % upper border
         A(k, k + Ny) = a; % right
         A(k, k + 1) = a; % down
         A(k, k - Ny) = a; %left
         A(k, k + Ny - 1) = a; %up
+        A(k, k + 2*Ny - 1) = b; % tr
+        A(k, k + Ny + 1) = b; % br
+        A(k, k - Ny + 1) = b; % bl
+        A(k, k - 1) = b; % tl
+        
         F(k, k + Ny) = q; % right
         F(k, k + 1) = q; % down
         F(k, k - Ny) = q; %left
@@ -76,6 +96,11 @@ for k = Ny+1 : (Nx-1)*Ny
         A(k, k - Ny + 1) = a; % down
         A(k, k - Ny) = a; %left
         A(k, k - 1) = a; %up
+        A(k, k + Ny - 1) = b; % tr
+        A(k, k + 1) = b; % br
+        A(k, k - 2*Ny + 1) = b; % bl
+        A(k, k - Ny - 1) = b; % tl
+        
         F(k, k + Ny) = q; % right
         F(k, k - Ny + 1) = q; % down
         F(k, k - Ny) = q; %left
@@ -85,6 +110,11 @@ for k = Ny+1 : (Nx-1)*Ny
         A(k, k + 1) = a; % down
         A(k, k - Ny) = a; %left
         A(k, k - 1) = a; %up
+        A(k, k + Ny - 1) = b; % tr
+        A(k, k + Ny + 1) = b; % br
+        A(k, k - Ny + 1) = b; % bl
+        A(k, k - Ny - 1) = b; % tl
+        
         F(k, k + Ny) = q; % right
         F(k, k + 1) = q; % down
         F(k, k - Ny) = q; %left
@@ -94,22 +124,32 @@ end
 
 %% X Right border
 % X Right border: first
-A((Nx-1)*Ny + 1, 1) = a; % right
-A((Nx-1)*Ny + 1, (Nx-1)*Ny + 1 + 1) = a; % down
-A((Nx-1)*Ny + 1, (Nx-1)*Ny + 1 - Ny) = a; %left
-A((Nx-1)*Ny + 1, N) = a; %up
-F((Nx-1)*Ny + 1, 1) = q; % right
-F((Nx-1)*Ny + 1, (Nx-1)*Ny + 1 + 1) = q; % down
-F((Nx-1)*Ny + 1, (Nx-1)*Ny + 1 - Ny) = q; %left
-F((Nx-1)*Ny + 1, N) = q; %up
+A(N - Ny + 1, 1) = a; % right
+A(N - Ny + 1, N - Ny + 1 + 1) = a; % down
+A(N - Ny + 1, N - Ny + 1 - Ny) = a; %left
+A(N - Ny + 1, N) = a; %up
+A(N - Ny + 1, Ny) = b; % tr
+A(N - Ny + 1, 2) = b; % br
+A(N - Ny + 1, N - Ny + 1 - Ny + 1) = b; % bl
+A(N - Ny + 1, N - Ny) = b; % tl
+
+F(N - Ny + 1, 1) = q; % right
+F(N - Ny + 1, N - Ny + 1 + 1) = q; % down
+F(N - Ny + 1, N - Ny + 1 - Ny) = q; %left
+F(N - Ny + 1, N) = q; %up
 
 % X Right border: intermediate
-for k = (Nx-1)*Ny+2 : N-1
+for k = N - Ny+2 : N-1
     % a coeffs
     A(k, mod(k, Ny)) = a; % right
     A(k, k + 1) = a; % down
     A(k, k - Ny) = a; %left
     A(k, k - 1) = a; %up
+    A(k, mod(k, Ny) - 1) = b; % tr
+    A(k, mod(k, Ny) + 1) = b; % br
+    A(k, k - Ny + 1) = b; % bl
+    A(k, k - Ny - 1) = b; % tl
+    
     F(k, mod(k, Ny)) = q; % right
     F(k, k + 1) = q; % down
     F(k, k - Ny) = q; %left
@@ -121,8 +161,18 @@ A(N, Ny) = a; % right
 A(N, N - Ny + 1) = a; % down
 A(N, N - Ny) = a; %left
 A(N, N - 1) = a; %up
+A(N, Ny - 1) = b; % tr
+A(N, 1) = b; % br
+A(N, N - 2*Ny + 1) = b; % bl
+A(N, N - Ny - 1) = b; % tl
+
 F(N, Ny) = q; % right
 F(N, N - Ny + 1) = q; % down
 F(N, N - Ny) = q; %left
 F(N, N - 1) = q; %up
+
+%% Solution
+rhs = f(params.x, params.y);
+u = A \ rhs(:);
+u = reshape(u, Ny, Nx);
 end

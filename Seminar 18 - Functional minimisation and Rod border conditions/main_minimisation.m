c806efabc3bfd98718d77ae82a7c6a0af97edf08
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2022-06-08 16:19:54 +0200

    [Fix] x as column vector

--- Seminar 18 - Functional minimisation and Rod border conditions/main_minimisation.m
+++ Seminar 18 - Functional minimisation and Rod border conditions/main_minimisation.m
@@ -8,23 +8,24 @@ theta = @(x) ones(size(x));
 f = @(x) sin(x);
 u_c = @(x, c1, c2) c1 + c2.*x - sin(x);
 
-F = @(x, u, du) theta(x).*du.^2/2 + f(x).*u(x);
+F = @(x, u, du) theta(x).*du.^2/2 + f(x).*u;
 
 Nx = 105;
 x = linspace(0, L, Nx);
+x = x(:);
 h = x(2) - x(1);
 
 %% Divergent scheme
 xh = .5*(x(2:end) + x(1:end-1));
 th = theta(xh);
 
-D = -diag([0 th] + [th 0]) + ...
+D = -diag([0; th] + [th; 0]) + ...
     diag(th, 1) + ...
     diag(th, -1);
 D(1, :) = 0; D(1, 1) = 1;
 D(end, :) = 0; D(end, end) = 1;
 
-rhs = h^2*f(x.');
+rhs = h^2*f(x);
 rhs(1) = uA; rhs(end) = uB;
 
 u_ds = D \ rhs;

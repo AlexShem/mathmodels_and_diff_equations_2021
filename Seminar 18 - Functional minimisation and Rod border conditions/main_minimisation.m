99eade7537c6fe2c82a5ca4e0090ad3cb60e5cc4
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2022-06-08 13:48:29 +0200

    [Upd] added true solution for theta=1

--- Seminar 18 - Functional minimisation and Rod border conditions/main_minimisation.m
+++ Seminar 18 - Functional minimisation and Rod border conditions/main_minimisation.m
@@ -4,7 +4,9 @@ uB = 0; % Right border value
 L = 2*pi;
 
 theta = @(x) ones(size(x));
+% theta = @(x) x;
 f = @(x) sin(x);
+u_c = @(x, c1, c2) c1 + c2.*x - sin(x);
 
 F = @(x, u, du) theta(x).*du.^2/2 + f(x).*u(x);
 

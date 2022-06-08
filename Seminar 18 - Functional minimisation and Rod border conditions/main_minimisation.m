4837cbb63befad83361c1cf924a39c3990edd986
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2022-06-08 13:48:03 +0200

    [Fix] Added parameters

--- Seminar 18 - Functional minimisation and Rod border conditions/main_minimisation.m
+++ Seminar 18 - Functional minimisation and Rod border conditions/main_minimisation.m
@@ -1,8 +1,13 @@
 %% Main parameters
-A = 0; % Left border value
-B = 0; % Right border value
+uA = 0; % Left border value
+uB = 0; % Right border value
+L = 2*pi;
 
 theta = @(x) ones(size(x));
 f = @(x) sin(x);
 
 F = @(x, u, du) theta(x).*du.^2/2 + f(x).*u(x);
+
+Nx = 105;
+x = linspace(0, L, Nx);
+h = x(2) - x(1);

7993830cc25af77af3fec6a68c7eaa2a34591247
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2022-06-12 18:50:24 +0200

    [Upd] adaptive lam multiplier

--- Seminar 19 - Function minimisation/grad_descent.m
+++ Seminar 19 - Function minimisation/grad_descent.m
@@ -1,15 +1,16 @@
-function [fmin, niter, path] = grad_descent(x0, f, tol, maxiter)
-if nargin < 3
-    tol = 1e-6;
-end
+function [xmin, fmin, niter, path] = grad_descent(x0, f, tol, maxiter)
 if nargin < 4
     maxiter = 50;
+end
+if nargin < 3 || (nargin == 4 && isempty(tol))
+    tol = 1e-8;
 end
 
 x0 = x0(:);
 path = zeros(length(x0), maxiter + 1);
 path(:, 1) = x0;
 
+lam = 1;
 grad_x = grad_f(x0, f);
 x = x0 - grad_x;
 iter = 1;
@@ -19,11 +20,14 @@ df = abs(f(x) - f(x0));
 
 while (df > tol) && iter < maxiter
     grad_x = grad_f(x, f);
-    grad_x0 = grad_f(x0, f);
-    lam = abs((x - x0).' * (grad_x - grad_x0)) / norm(grad_x - grad_x0, 2);
-
+    
     x0 = x;
-    x = x0 - lam*grad_x;
+    x = x0 - lam * grad_x/norm(grad_x, 2);
+    while f(x) > f(x0)
+        lam = lam/2;
+        x = x0 - lam * grad_x/norm(grad_x, 2);
+    end
+    
     iter = iter + 1;
     df = abs(f(x) - f(x0));
     path(:, iter + 1) = x;
@@ -32,6 +36,7 @@ end
 if iter == maxiter && df > tol
     warning('Gradient descent algorithm did not converge properly');
 end
+xmin = x;
 fmin = f(x);
 niter = iter;
 path = path(:, 1 : niter + 1);
--- Seminar 19 - Function minimisation/main.m
+++ Seminar 19 - Function minimisation/main.m
@@ -2,17 +2,18 @@
 
 f = @(x, y) x.*exp(-x.^2-y.^2)+(x.^2+y.^2)/20; % Function to minimise
 figure(1)
-fsurf(f, [-2, 2], 'ShowContours', 'on')
+fsurf(f, [-2, 2], 'ShowContours', 'on', 'FaceAlpha', .3)
 xlabel('x');
 ylabel('y')
 
 fx = @(x) x(1).*exp(-x(1).^2-x(2).^2)+(x(1).^2+x(2).^2)/20;
 
 %% Gradient descent
-x0 = [1; 1];
-[fmin, niter, path] = grad_descent(x0, fx)
+x0 = [1.5; 1.5];
+[xmin, fmin, niter, path] = grad_descent(x0, fx, [], 1000)
 
 figure(1)
 hold on;
 plot3(path(1,:), path(2,:), f(path(1,:), path(2,:)), '-*r')
 hold off;
+

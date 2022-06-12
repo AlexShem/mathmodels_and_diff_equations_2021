e8208c76342ae2a62ad26659f0106fe342252c51
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2022-06-12 21:31:52 +0200

    [Upd] update of gradient example

--- Seminar 19 - Function minimisation/main.m
+++ Seminar 19 - Function minimisation/main.m
@@ -1,19 +1,21 @@
 %% Main parameters
-
 f = @(x, y) x.*exp(-x.^2-y.^2)+(x.^2+y.^2)/20; % Function to minimise
+fx = @(x) x(1).*exp(-x(1).^2-x(2).^2)+(x(1).^2+x(2).^2)/20;
+lb = [-2, -2];
+ub = [2, 2];
+
 figure(1)
-fsurf(f, [-2, 2], 'ShowContours', 'on', 'FaceAlpha', .3)
+fsurf(f, [lb(1) ub(1) lb(2) ub(2)], 'ShowContours', 'on', 'FaceAlpha', .3)
 xlabel('x');
 ylabel('y')
 
-fx = @(x) x(1).*exp(-x(1).^2-x(2).^2)+(x(1).^2+x(2).^2)/20;
-
 %% Gradient descent
 x0 = [1.5; 1.5];
-[xmin, fmin, niter, path] = grad_descent(x0, fx, [], 1000)
+% x0 = [0; 0];
+[xmin_g, fmin_g, niter_g, path_g] = grad_descent(x0, fx, [], 1000);
 
 figure(1)
 hold on;
-plot3(path(1,:), path(2,:), f(path(1,:), path(2,:)), '-*r')
+plot3(path_g(1,:), path_g(2,:), f(path_g(1,:), path_g(2,:)), '-*r')
 hold off;
 
